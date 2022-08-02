7f2989fd53e890c38d8b58a890cbb1b2589b5f6b
Rakezt <keztmckinnon@gmail.com>
2022-08-02 01:48:12 -0400

    init html and script

---
+++ script.js
@@ -0,0 +1,24 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputText=document.querySelector("#output-text");
+//var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverUrl="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslation(text){
+    return serverUrl+"?"+"text="+text;
+}
+function errorhandler(error){
+    console.log("error occured", error)
+}
+
+function clickHandler(){
+    var inputText=txtInput.value;
+
+    fetch(getTranslation(inputText))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedText=json.contents.translated;
+        outputText.innerText=translatedText;})
+    .catch(errorhandler)
+};
+btnTranslate.addEventListener("click", clickHandler)
